c6cbd8f1bc83598199851f14364a62443bb99550
thundr2006 <robert.ratheram@mail.bcu.ac.uk>
2026-09-24 12:23:44 +0100

    Update Button.cs

--- learnbuildingmobile/buildtoandroid/Assets/Scripts/Button.cs
+++ learnbuildingmobile/buildtoandroid/Assets/Scripts/Button.cs
@@ -5,7 +5,14 @@ public class Button : MonoBehaviour
 {
     public Image buttonImage;
 
-    public void OnButtonPress() {
-        
+    private bool active = false;
+
+    public void OnMouseDown() {
+        active = !active;
+        if (active) {
+            buttonImage.color = new Color(1f, 0f, 0f, 1f);
+        } else {
+            buttonImage.color = new Color(1f, 1f, 1f, 1f);
+        }
     }
 }
